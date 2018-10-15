d3216d7256631bfe86408f7728ecc9c1ac3fce38
segauthier <segauthier@groupeseb.com>
2018-10-15 17:10:42 +0200

    adding sleep before receiving

--- socket_server.py
+++ socket_server.py
@@ -46,7 +46,7 @@ class ClientThread(threading.Thread):
         print("Starting new client thread at %s, port: %s" % (self.ip, self.port, ))
 
     def run(self):
-        time.sleep(5)
+        time.sleep(10)
         bool_run = True
         while bool_run:
             response = self.recv_timeout(10)
